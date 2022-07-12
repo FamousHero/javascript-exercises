3fd72c616de8e9cbd9b03265d489b5073a21f0a1
Daniel F <danielfranco2011@gmail.com>
2022-07-12 14:13:50 -0700

    JS exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let ans = ``;
+    for(let i = string.length-1; i >= 0; --i)
+        ans += string[i];
+    return ans;
 };
+//why split-reverse-join???
 
 // Do not edit below this line
 module.exports = reverseString;
