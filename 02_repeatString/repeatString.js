030e0668bd6cd5d6212037db853b30576976c28f
Daniel F <danielfranco2011@gmail.com>
2022-07-12 14:01:57 -0700

    JS exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, reps) {
+    let ans = ``;
+    if(reps < 0)
+        return `ERROR`;
+    for(let i = 0; i < reps; ++i)
+        ans += string;
+    return ans;
 };
 
 // Do not edit below this line
