30e6bbcbfa9bfcb8d3491bddd81505725167c70f
Daniel F <danielfranco2011@gmail.com>
2022-07-12 14:45:30 -0700

    JS exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(startindex, endindex) {
+    let ans = 0;
+//can use typeof or isNumber()
+    if(typeof(startindex) !=  'number' 
+        || typeof(endindex) != 'number')
+        return `ERROR`;
+    if(startindex < 0 || endindex < 0)
+        return `ERROR`
+    if(startindex > endindex)
+    {
+        let temp = endindex;
+        endindex = startindex;
+        startindex = temp;
+    }
+    for(let i = startindex; i <= endindex; ++i)
+        ans += i;
+    return ans;
 };
 
 // Do not edit below this line
