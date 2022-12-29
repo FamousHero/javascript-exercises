47ea5ec2121d7268fcd99ea2ed20730ad5c157d2
Daniel Franco <danielfranco2011@gmail.com>
2022-12-28 20:53:53 -0800

    JS exercise 07

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(a,b) {
+  return a-b;
 
 };
 
-const power = function() {
-	
+const sum = function(a) {
+	return a.reduce((total, elem)=>{return total+elem},0);
 };
 
-const factorial = function() {
-	
+const multiply = function(a) {
+  return a.length === 0? 0: a.reduce((total, elem)=>{return total*elem});
+};
+
+const power = function(a,b) {
+	let ans=1;
+  for(let i =0; i < b; ++i)
+  {
+    ans *= a;
+  }
+  return ans;
+};
+
+const factorial = function(a) {
+	if(a <= 1){
+    return 1;
+  }
+  return a * factorial(a-1);
 };
 
 // Do not edit below this line
