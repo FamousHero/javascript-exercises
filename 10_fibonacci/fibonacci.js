f33c2079a50b30aae0d391ea47acc4ea74f4f6f7
Daniel Franco <danielfranco2011@gmail.com>
2022-12-28 21:30:26 -0800

    JS Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(depth) {
+    let i = 0; let j =1;
+    if(depth <= 0)
+    return "OOPS";
+    for(let k = 0; k < depth; ++k)
+    {
+        let temp = j;
+        j = i+j;
+        i = temp;
+        console.log(i);
+    }
+    return i;
 };
 
 // Do not edit below this line
