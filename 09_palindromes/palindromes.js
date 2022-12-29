080fa87ca8505f630b9c028597de708329acaa12
Daniel Franco <danielfranco2011@gmail.com>
2022-12-28 21:14:32 -0800

    JS exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const parsedS = s.toLowerCase().replace(/[^a-z]/g, "");
+    let left = 0;
+    let right = parsedS.length-1;
+    while(left <= right)
+    {
+        if(parsedS[left++] !== parsedS[right--])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
